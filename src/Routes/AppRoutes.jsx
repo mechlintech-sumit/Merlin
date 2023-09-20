2535551efaac525dcdbd2e72916aa2cf05b25f67
mechlintech-sumit <sumit.kumar@mechlintech.com>
2023-09-20 16:06:17 +0530

    Code Modification

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "../pages/Authentication/HomePage";
+import HomePage from "../pages/HomePage/HomePage";
 import { Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
